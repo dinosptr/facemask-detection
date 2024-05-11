3715214f34b4aade66c29cb12162e1acb6eb109e
Achmad Dino Saputra <achmadino2015@gmail.com>
2024-05-11 21:15:07 +0700

    add emoji

--- Welcome.py
+++ Welcome.py
@@ -5,7 +5,7 @@ def main():
   st.write("Welcome to the face mask detection app! This app helps you detect whether someone in an image is wearing a face mask correctly or not.")
 
   st.header("Problem Statement")
-  st.subheader("Problem Statements :thinking:")
+  st.subheader("Problem Statements :thinking_face:")
   st.write("In the midst of the COVID-19 pandemic, proper face mask usage has become crucial to prevent the spread of the virus. However, many people still do not wear masks correctly, such as not covering their noses and mouths completely.")
 
   st.subheader("Goals :dart:")
